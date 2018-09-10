b371cd9a8098e3304f6ecef9c018427783badf6f
araujoDionata <araujo.dionata@gmail.com>
2018-09-10 14:12:55 -0300

    Inserção de usuário com POST

--- src/main/java/com/dionatamartins/workshopmongodb/services/UserService.java
+++ src/main/java/com/dionatamartins/workshopmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dionatamartins.workshopmongodb.domain.User;
+import com.dionatamartins.workshopmongodb.dto.UserDTO;
 import com.dionatamartins.workshopmongodb.repository.UserRepository;
 import com.dionatamartins.workshopmongodb.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findByid(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
