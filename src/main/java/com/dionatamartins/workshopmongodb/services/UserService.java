bbfcf1ee6df8031c5f215829c26f47eecfd06e1b
araujoDionata <araujo.dionata@gmail.com>
2018-09-10 14:26:26 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/dionatamartins/workshopmongodb/services/UserService.java
+++ src/main/java/com/dionatamartins/workshopmongodb/services/UserService.java
@@ -21,7 +21,7 @@ public class UserService {
 		return repo.findAll();
 	}
 
-	public User findByid(String id) {
+	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
@@ -29,8 +29,15 @@ public class UserService {
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
 
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
+	
+	
 }
