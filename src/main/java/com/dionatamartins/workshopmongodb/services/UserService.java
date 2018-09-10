0ab2e0c8da3f1af588af76ab785f782c7d4274cc
araujoDionata <araujo.dionata@gmail.com>
2018-09-10 13:53:03 -0300

    Obtendo um usuário por id

--- src/main/java/com/dionatamartins/workshopmongodb/services/UserService.java
+++ src/main/java/com/dionatamartins/workshopmongodb/services/UserService.java
@@ -1,20 +1,27 @@
 package com.dionatamartins.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dionatamartins.workshopmongodb.domain.User;
 import com.dionatamartins.workshopmongodb.repository.UserRepository;
+import com.dionatamartins.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findByid(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
