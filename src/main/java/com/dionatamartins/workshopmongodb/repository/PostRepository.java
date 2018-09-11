0784fd9b16438b84d36f5ae3239b086ee8bcf2ea
araujoDionata <araujo.dionata@gmail.com>
2018-09-11 09:01:20 -0300

    Consulta simples com @Query

--- src/main/java/com/dionatamartins/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/dionatamartins/workshopmongodb/repository/PostRepository.java
@@ -3,14 +3,20 @@ package com.dionatamartins.workshopmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.dionatamartins.workshopmongodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-	
-	//QueryMethods do MongoDB
+
+	// QueryMethods do MongoDB
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
+	// https://docs.mongodb.com/manual/reference/operator/query/regex/
+	// ?0 = primeiro parametro do metodo (String text)
+	// i = option camel case ignore
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 }
