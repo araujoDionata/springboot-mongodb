91e1df4f369ce23c47bef14da4fb34399091f634
araujoDionata <araujo.dionata@gmail.com>
2018-09-11 08:43:45 -0300

    Consulta simples com query methods

--- src/main/java/com/dionatamartins/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/dionatamartins/workshopmongodb/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.dionatamartins.workshopmongodb.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,8 @@ import com.dionatamartins.workshopmongodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	//QueryMethods do MongoDB
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
